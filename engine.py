01e53b85f6ad6057ed0ec361e24780de320498cb
amalkov <amalkov@amalkov.(none)>
2012-03-29 07:03:21 +0400

    some experiments done

--- engine.py
+++ engine.py
@@ -1,17 +1,38 @@
 import os, sys
 from pyquery import PyQuery
 
+keys= [
+''
+'rooms',
+'address',
+'level',
+'building'
+]
+
+def mkresult(el):
+	obj = {}
+
+	def add(i, el):
+		if i < 5:
+			key = keys[i]
+			if key:
+				obj[key] = PyQuery(el).text() 
+		
+	klass = el.attr('class')
+	if klass == 'bg1' or klass == 'bg2':
+		tds = el.find('td')
+		tds.each(lambda i, el: add(i, el))
+
 def find(rfrom, rto, pfrom, pto, metro):
 	url = 'http://www.bn.ru/zap_fl.phtml?kkv1=%s&kkv2=%s&price1=%s&price2=%s&so1=&so2=&sk1=&sk2=&type[]=1&sorttype=0&sort_ord=0&metro[]=%s&text=' % (rfrom, rto, pfrom, pto, metro)
 	pq = PyQuery(url=url, opener=lambda url: load(url))
 	results = pq('table.results tr')
-	return [PyQuery(el).text() for el in results]
+	return [mkresult(PyQuery(el)) for el in results]
 
 def getmetro():
 	url = 'http://www.bn.ru/zap_fl_w.phtml'
 	pq = PyQuery(url=url, opener=lambda url: load(url))
 	options = pq('select#metro option')
-	#
 	def mkobj(el):
 		name = el.text()
 		value = el.attr('value')
