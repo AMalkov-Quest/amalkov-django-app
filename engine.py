396448b394425b6167a47745972535ce8a1f57ef
Andrew Malkov <andrey.malkov@quest.com>
2012-03-29 11:37:56 +0400

    extending data

--- engine.py
+++ engine.py
@@ -2,32 +2,55 @@ import os, sys
 from pyquery import PyQuery
 
 keys= [
-''
+'',
 'rooms',
 'address',
 'level',
-'building'
+'building',
+'common',
+'living',
+'kitchen'
 ]
 
-def mkresult(el):
+def mkresult(el, delimiter='td'):
+	"""
+	>>> el = PyQuery("<tr class='bg1'><td></td><td>1</td><td>qwerty</td></tr>")
+	>>> mkresult(el)
+	{'rooms': '1', 'address': 'qwerty'}
+	"""
 	obj = {}
+	def _add(i, el):
+		if i < 8:
+			add(i, el)
 
 	def add(i, el):
-		if i < 5:
-			key = keys[i]
-			if key:
-				obj[key] = PyQuery(el).text() 
+		key = keys[i]
+		if key:
+			pyel = PyQuery(el)
+			obj[key] = pyel.text()
+
+	def addbg3(i, el):
+		if i > 0:
+			i += 1
+		if i < 8:
+			add(i, el)
 		
 	klass = el.attr('class')
+	tds = el.find(delimiter)
 	if klass == 'bg1' or klass == 'bg2':
-		tds = el.find('td')
-		tds.each(lambda i, el: add(i, el))
+		tds.each(lambda i, el: _add(i, el))
+	elif klass == 'bg3':
+		tds.each(lambda i, el: addbg3(i, el))
+
+	return obj
 
 def find(rfrom, rto, pfrom, pto, metro):
 	url = 'http://www.bn.ru/zap_fl.phtml?kkv1=%s&kkv2=%s&price1=%s&price2=%s&so1=&so2=&sk1=&sk2=&type[]=1&sorttype=0&sort_ord=0&metro[]=%s&text=' % (rfrom, rto, pfrom, pto, metro)
 	pq = PyQuery(url=url, opener=lambda url: load(url))
 	results = pq('table.results tr')
-	return [mkresult(PyQuery(el)) for el in results]
+	header = mkresult(PyQuery(results[0]), 'th')
+	data = [mkresult(PyQuery(el)) for el in results]
+	return filter(None, [header]+data)
 
 def getmetro():
 	url = 'http://www.bn.ru/zap_fl_w.phtml'
@@ -48,6 +71,5 @@ def load(url):
 	return "".join(data)
 
 if __name__ == '__main__':
-	#print load('http://www.bn.ru/zap_fl_w.phtml')
-	#getmetro()
-	find(1, 2, 1000, 2000, 52)
+	import doctest
+	doctest.testmod()
